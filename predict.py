35d528f0958af57ff9071b2bbedb68d0d563d935
yizt <csuyzt@163.com>
2019-05-03 10:37:32 +0800

    east/utils/common_utils.py

--- predict.py
+++ predict.py
@@ -28,19 +28,23 @@ def main(args):
     # 加载图片
     image = image_utils.load_image(args.image_path)
     image, image_meta, _ = image_utils.resize_image_and_gt(image,
-                                                           config.input_shape[0])
+                                                           config.IMAGE_SHAPE[0])
 
     # 加载模型
-    m = models.ctpn_net(config, 'test')
+    m = models.east_net(config, 'test')
     m.load_weights(config.WEIGHT_PATH, by_name=True)
     # m.summary()
 
     # 模型预测
     predict_scores, predict_vertex, image_metas = m.predict([np.array([image]), np.array([image_meta])])
+
     # 后处理
     polygons = common_utils.relative_to_absolute(predict_vertex)[0]  # 相对坐标转绝对坐标
     polygons *= 4  # 转为网络输入的大小
     scores = predict_scores[0]
+    # 高度和宽度打平
+    polygons = np.reshape(polygons, (-1, 4, 2))
+    scores = np.reshape(scores, (-1,))
     # 过滤低分值多边形
     ix = np.where(scores >= 0.5)
     polygons = polygons[ix]
@@ -48,12 +52,12 @@ def main(args):
     polygons, scores = common_utils.locale_aware_nms(polygons, scores, 0.3)
 
     # 可视化保存图像
-    fig = plt.figure(figsize=(16, 16))
+    fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(1, 1, 1)
-    visualize.display_polygons(image, polygons.reshape((-1, 8)), scores,
+    visualize.display_polygons(image, polygons, scores,
                                ax=ax)
     image_name = os.path.basename(args.image_path)
-    fig.savefig('{}.{}.jpg'.format(os.path.splitext(image_name)[0], int(config.USE_SIDE_REFINE)))
+    fig.savefig('{}.jpg'.format(os.path.splitext(image_name)[0]))
 
 
 if __name__ == '__main__':
